b1f6d957ce0be4ceb431418627a03f6e6e4062bd
kbogdev <kbogdev@gmail.com>
2015-12-21 11:35:18 -0800

    'Red - test is failing

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class FareyNumber
+    {
+        public readonly uint N;
+        public readonly uint D;
+
+        public FareyNumber(uint n, uint d)
+        {
+            N = n;
+            D = d;
+        }
+
+        public override string ToString()
+        {
+            string result = String.Format("{0}/{1}", N, D);
+            return result;
+        }
+    }
+
+    public class FareyNumbers
+    {
+        public readonly uint Num;
+        public readonly uint Count;
+
+        HashSet<FareyNumber> Numbers = new HashSet<FareyNumber>();
+
+        public FareyNumbers(uint N)
+        {
+            Num = N;
+            Count = 0;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FareyNumbers numbers = new FareyNumbers(4);
+            //Console.WriteLine("N={0}, FareyNumbers={1}", numbers.Num, numbers.ToString());
+            Console.WriteLine("FareyNumber={0}", new FareyNumber(1,2));
+        }
+    }
+}
---
+++ UnitTestApp1/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ConsoleApp1;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestApp1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            FareyNumbers nums = new FareyNumbers(1);
+            Assert.IsTrue(nums.Count == 2);
+        }
+        public void TestMethod2()
+        {
+            FareyNumbers nums = new FareyNumbers(2);
+            Assert.IsTrue(nums.Count == 3);
+        }
+    }
+}
